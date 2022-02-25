462793616bc49b1f33cc9760f7dcce6fa9d2bbe2
Steve Harmeyer <harmeyersteve@gmail.com>
2022-02-25 07:54:37 -0700

    merge with laravel app

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use LaravelJsonApi\Core\Exceptions\JsonApiException;
+use LaravelJsonApi\Exceptions\ExceptionParser;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        JsonApiException::class,
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->renderable(ExceptionParser::make()->renderable());
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+}
