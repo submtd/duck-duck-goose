c8d2f221f009e937129437b39c258e754f7c171a
Steve Harmeyer <harmeyersteve@gmail.com>
2022-02-27 21:52:54 -0700

    mint dapp

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'app.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "src/*.html",
+                    to: "[name].html",
+                },
+            ],
+        }),
+        new NodePolyfillPlugin({
+            excludeAliases: ['console'],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
