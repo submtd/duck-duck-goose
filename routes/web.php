af49b63696acbf0c08104c4f50b6bbf2fa733714
Steve Harmeyer <harmeyersteve@gmail.com>
2022-03-02 21:52:23 -0700

    created discord link

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('mint');
 });
+
+Route::get('discord', function () {
+    return response()->redirectTo('https://discord.gg/PN4KuTNRCv');
+});
