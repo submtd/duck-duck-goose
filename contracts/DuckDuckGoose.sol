514e4d2ad3e2de8580c0b3366a08c6c7c757c896
Steve Harmeyer <harmeyersteve@gmail.com>
2022-03-02 22:18:17 -0700

    updated contract

--- contracts/DuckDuckGoose.sol
+++ contracts/DuckDuckGoose.sol
@@ -9,7 +9,30 @@ import "./Egg.sol";
 import "./Duck.sol";
 import "./Goose.sol";
 
-contract DuckDuckGoose is Ownable, ERC721 {
+abstract contract ContextMixin {
+    function msgSender()
+        internal
+        view
+        returns (address payable sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = payable(msg.sender);
+        }
+        return sender;
+    }
+}
+
+contract DuckDuckGoose is Ownable, ERC721, ContextMixin {
     /**
      * Item types.
      * @dev all items start out as an egg, then become either a duck or a goose.
@@ -115,6 +138,33 @@ contract DuckDuckGoose is Ownable, ERC721 {
      * @dev this fires when a goose hatches.
      */
     event GooseHatched(uint256 goose, address gooseOwner, uint256 prize);
+
+    /**
+    * Override isApprovedForAll to auto-approve OS's proxy contract
+    */
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public override view returns (bool isOperator) {
+        // if OpenSea's ERC721 Proxy Address is detected, auto-return true
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+        // otherwise, use the default ERC721.isApprovedForAll()
+        return ERC721.isApprovedForAll(_owner, _operator);
+    }
+
+    /**
+    * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+    */
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address sender)
+    {
+        return ContextMixin.msgSender();
+    }
 
     /**
      * Mint.
