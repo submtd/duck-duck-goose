dcfb55024ddec8e3c8a611a30fb0f475e4dff547
Steve Harmeyer <harmeyersteve@gmail.com>
2022-03-03 10:25:52 -0700

    add analytics

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,18 @@ import Mint from "./components/Mint";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
+import Analytics from 'analytics';
+import googleTagManager from '@analytics/google-tag-manager';
+
+window.analytics = Analytics({
+    app: 'duckduckgoose.club',
+    plugins:  [
+        googleTagManager({
+            containerId: 'GTM-PVG2FPX',
+        }),
+    ],
+});
+
 const app = createApp({});
 app.component('mint', Mint);
 app.mount('#app');
